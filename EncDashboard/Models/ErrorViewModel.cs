d8a599abada84327817f19266487ce79f1c6605f
Hashir Nadeem <hashoonadeem123@gmail.com>
2023-09-15 13:29:13 +0500

    Project Initialized

---
+++ EncDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EncDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
